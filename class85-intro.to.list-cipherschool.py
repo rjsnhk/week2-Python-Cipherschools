a0f59a4eac53b5c8708ba13751af2626082cab2c
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2022-12-12 02:33:16 +0530

    Add files via upload

---
+++ class85-intro.to.list-cipherschool.py
@@ -0,0 +1,17 @@
+n1=[1,2,3,4]
+print(n1)
+
+w=['raj','ram','hari']
+print(w)
+
+mix=[1,2,3,'hari',3.4,'ram']
+print(mix)
+#replace ho rha under wala
+# mix[1]='two'
+# print(mix)
+
+# mix[1:]='two'
+# print(mix)
+
+mix[1:]=['two','raj']
+print(mix)
