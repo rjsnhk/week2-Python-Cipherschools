f81f3c3e16c7fe23b446e25d9a63bd3585d57c9c
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2022-12-11 20:02:52 +0530

    Add files via upload

---
+++ class83-default.parameter-cipherschool.py
@@ -0,0 +1,31 @@
+# def user_info(fname,lname,age):
+#     print(f'ur f name is{fname} ')
+#     print(f'ur l name is{lname} ')
+#     print(f'ur age is{age} ')
+    
+# user_info('rajesh','nahak',19)
+
+
+# def user_info(fname,lname,age=24):
+#     print(f'ur f name is{fname} ')
+#     print(f'ur l name is{lname} ')
+#     print(f'ur age is{age} ')
+    
+# user_info('rajesh','nahak',19) # it 19 overflow 24
+
+
+# def user_info(fname,lname='unknown',age=None):
+#     print(f'ur f name is{fname} ')
+#     print(f'ur l name is{lname} ')
+#     print(f'ur age is{age} ')
+    
+# user_info('rajesh')
+
+
+# def user_info(fname='unknown',lname,age):
+#     print(f'ur f name is{fname} ')
+#     print(f'ur l name is{lname} ')
+#     print(f'ur age is{age} ')
+    
+# user_info('nahak',19) 
+# # it does not work because last wala hi unknown ho skta he 
