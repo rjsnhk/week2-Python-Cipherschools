e0863ea7bc08664e4919a65e2b64f66ea1b21317
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2022-12-12 02:33:28 +0530

    Add files via upload

---
+++ class86-add.data.to.list-cipherschool.py
@@ -0,0 +1,11 @@
+# num=[1,2,3]
+# num.append(5)
+# print(num)
+# # add items in string using append jo bhi add hoga wo last me ayega bich me nhi
+
+
+fruits=[]
+fruits.append('apple')
+fruits.append('mango')
+fruits.append('banana')
+print(fruits)
