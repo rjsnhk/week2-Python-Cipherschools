0bd83265542fae9a5ae2a01d406206d91e0a9474
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2022-12-12 02:33:39 +0530

    Add files via upload

---
+++ class87-more.method.to.add-cipherschool.py
@@ -0,0 +1,21 @@
+# #insert method  jo jagah pe usse add krna he wha kro
+# f1=['apple','orange']
+# f1.insert(0,'banana')
+# print(f1)
+
+# join two variable in one string
+# f1=['apple','orange']
+# f2=['banana','grapes']
+# print(f1+f2)
+
+#extend method add krne jesa
+# f1=['apple','orange']
+# f2=['banana','grapes']
+# f1.extend(f2)
+# print(f1)
+
+# append method but yha string bracket ke sath add hoga
+f1=['apple','orange']
+f2=['banana','grapes']
+f1.append(f2)
+print(f1)
