72c5e7a8fdd30bda5e4efe7793356052736d810f
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2022-12-12 02:33:53 +0530

    Add files via upload

---
+++ class88-delete.data.list-cipherschool.py
@@ -0,0 +1,23 @@
+fruit=['apple','banana','apple','mango','kiwi']
+
+#pop method
+# fruit.pop() it cuts last object
+# fruit.pop(2) it cuts mango
+# print(fruit)
+
+# #del method
+# del fruit[2]
+# print(fruit)
+
+# remove method
+# fruit.remove('banana')
+# print(fruit)
+
+# fruit.remove('apple') it cuts 1st apple
+# print(fruit)
+
+
+
+# for add---> + ,insert , append ,extend
+
+# for delete---> pop , del , remove
