66791162ec8e3858a41124fd590f06634dd5ebeb
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2022-12-12 14:39:54 +0530

    Add files via upload

---
+++ class92-join.split-cipherschool.py
@@ -0,0 +1,23 @@
+#split method
+# name=' rajesh 09 03'.split()
+# print(name)
+
+# name='rajesh,09,03'.split(',')
+# print(name)
+
+# name,age='rajesh,09'.split(',')
+# print(name)
+# print(age)
+
+# name,age=input('name & age separeted by comma').split(',')
+# print(name)
+# print(age)
+
+
+# join method
+# name='rajesh'
+# print(','.join(name))
+
+# # here int bhi string convert krna he
+# name=['rajesh','24']  
+# print(','.join(name))
