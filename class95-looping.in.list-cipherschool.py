91fc7dba50809b6617a0d18a08f21928976d86f8
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2022-12-12 22:07:58 +0530

    Add files via upload

---
+++ class95-looping.in.list-cipherschool.py
@@ -0,0 +1,10 @@
+fruits=['apple','banana','apple','mango','kiwi','orange']
+
+# for fruit in fruits:     #fruit work like i,j in for loop
+#     print(fruit)
+    
+    
+i=0
+while i<len(fruits):
+    print(fruits[i])
+    i+=1
