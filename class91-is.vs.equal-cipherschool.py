a8968edb1f3e3813c422287428bd643edcda9632
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2022-12-12 14:38:25 +0530

    Add files via upload

---
+++ class91-is.vs.equal-cipherschool.py
@@ -0,0 +1,8 @@
+f1=['apple','banana','apple','mango','kiwi','orange']
+f2=['mango','kiwi','orange']
+f3=['apple','banana','apple','mango','kiwi','orange']
+# print(f1 is f2)
+# print(f1 == f2)
+
+print(f1==f3)  # true reson--ye only values chech
+print(f1 is f3)  # false reson--ye object check krta he yha dono alag alag variable he
