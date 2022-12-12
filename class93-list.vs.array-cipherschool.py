2d933a080289fc0cf76a5ee22c8ed6099b1807bd
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2022-12-12 22:07:34 +0530

    Add files via upload

---
+++ class93-list.vs.array-cipherschool.py
@@ -0,0 +1,13 @@
+# c,c++,java
+# array use
+
+# python
+# list use or array module
+
+# list--store any data/flexible
+
+# python array module--fix data type
+
+# numpy array==binding with c
+
+# javascript array= py list
