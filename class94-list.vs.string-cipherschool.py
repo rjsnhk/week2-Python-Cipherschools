9720f3b1ba4321ba37851edc72993528d9bef763
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2022-12-12 22:07:45 +0530

    Add files via upload

---
+++ class94-list.vs.string-cipherschool.py
@@ -0,0 +1,19 @@
+# string--immutable
+# list--mutable
+
+
+s='string'
+# s.title()  #title first letter ko capital krega
+# print(s)
+# t=s.title()
+# print(t)
+
+# no add char on string but add on list
+
+
+
+l=['a','b','c','d']
+# l.pop(2)
+# print(l)
+l.append('Z')
+print(l)
