52adb04f48dcd3c549a3ee49d3c2c60034587f27
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2022-12-12 22:08:32 +0530

    Add files via upload

---
+++ class96-list.inside.list-cipherschool.py
@@ -0,0 +1,26 @@
+matrix=[[1,2,3],[4,5,6],[7,8,9]]   # list and sublist  ---2dlist aur hoga to 3d list
+# print(matrix[2])
+
+# for i in matrix:
+#     print(i)
+
+# for sublist in matrix:
+#     for i in sublist:
+#         print(i)
+
+
+# # output 5
+# print(matrix[1][1])
+
+# # output  7
+# print(matrix[2][0])
+
+
+# type function
+a=18
+b=9.65
+c='rajesh'
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(matrix))
