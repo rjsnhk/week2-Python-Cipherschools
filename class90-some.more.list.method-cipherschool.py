0c419be6aba56b79b88ddb743ea63e4f81961f63
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2022-12-12 14:35:43 +0530

    Add files via upload

---
+++ class90-some.more.list.method-cipherschool.py
@@ -0,0 +1,27 @@
+fruit=['apple','banana','apple','mango','kiwi','orange']
+
+# #count
+# print(fruit.count('apple'))
+
+#sort --- abcd wise rhega also number increasing order
+# fruit.sort()
+# print(fruit)
+
+#sorted function
+# print(sorted(fruit))
+
+
+# #clear form string
+# fruit.clear()
+# print(fruit)
+
+
+# #reverse
+# fruit.reverse()
+# print(fruit)
+
+
+f2=fruit.copy()
+print(fruit)
+print(f2)
+
