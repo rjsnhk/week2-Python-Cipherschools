5da76e1eb722e0606d3a15a994be8ba293529846
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2022-12-16 20:49:24 +0530

    Add files via upload

---
+++ class111-chap5.summary-cipherschool.py
@@ -0,0 +1,54 @@
+# list chap. summary
+word=['w1','w2']
+
+
+mixed=[1,2,3,[2,3,4],'words','sex',9.5,None]
+
+# print(mixed[0]) #out 1
+
+# print(mixed[3])
+
+# print(mixed[5])
+
+# add method
+
+# word.append(mixed)
+# print(word)
+
+# word.extend(mixed)
+# print(word)
+
+#add in a list
+# print(word,mixed)
+
+# join two list
+# print(word+mixed)
+
+# word.insert(1,'inserted')
+# print(word)  #['w1', 'inserted', 'w2']
+
+# data remove ke liye
+# word.pop()
+# print(word)
+
+# word.pop(0)
+# print(word)
+
+# p=word.pop()
+# print(p)
+
+# w=word.pop(0)
+# print(w)
+
+# remov statement
+# mixed.remove('sex')
+# print(mixed)
+
+# # del statement
+# del mixed[0]  #write position
+# print(mixed)
+
+# loop in list
+for i in mixed:
+    print(i)
+
