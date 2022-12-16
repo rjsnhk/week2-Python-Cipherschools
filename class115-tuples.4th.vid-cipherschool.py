09a6ec2c69eb442dd89295ed2f6cf83bebedc4bb
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2022-12-16 20:50:21 +0530

    Add files via upload

---
+++ class115-tuples.4th.vid-cipherschool.py
@@ -0,0 +1,12 @@
+# nums=tuple(range(1,10))
+# print(nums)
+# print(list(nums))
+# print(str(nums))  #pura tuple ko ek hi value string bna dia  out--'(1, 2, 3, 4, 5, 6, 7, 8, 9)'
+
+# num=str((1,2,3,4,4,5,5,5))  #red bracket tuple he
+## print(num)
+# print(type(num))
+
+num_list=str([1,2,3,4,5,6,])
+print(num_list)
+print(type(num_list))
