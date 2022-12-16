9b482292f0d2317adfd772ca1a21b4fb34316990
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2022-12-16 20:49:37 +0530

    Add files via upload

---
+++ class112-tuple.intro-cipherschool.py
@@ -0,0 +1,23 @@
+# tuple
+a=('apple','orange','banana')  
+#imutable ,no append,insert,pop,remove
+
+day=('monday','tuesday')
+# it use for no data change , it is very fast
+
+#methods-->count,index
+
+# len function 
+
+# slicing function
+# print(a[:2])
+
+# not change
+# a[0]=2
+# print(a)  no output
+
+# on list there is output
+# b=['apple',6,7,8]
+
+# b[0]=1
+# print(b)
