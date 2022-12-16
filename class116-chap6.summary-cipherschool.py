1b1036dff0494e4cf04f4eea0f679742f6ae97ab
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2022-12-16 20:50:32 +0530

    Add files via upload

---
+++ class116-chap6.summary-cipherschool.py
@@ -0,0 +1,28 @@
+# tuple summary
+# it is immutable
+# it is ordered collection of items
+# it ca store any data type
+# we cant change(add or delete) data from it
+# we can chande dATA FROM INside list of tupple
+
+a=('apple','orange','banana')  
+#imutable ,no append,insert,pop,remove
+# it use for no data change , it is very fast
+
+# funcn---> min,max,sum
+a=(1,2,3,4,5,)
+print(min(a))
+print(max(a))
+print(sum(a))
+print(len(a))
+
+
+# list inside tupple we can add , pop data from that list
+# fav=("apple",'boy',['a','cup',4,5])
+# # fav[2].pop()
+# # fav[2].append('we do it')
+# fav[2].extend('hey')
+# print(fav)
+
+day=('monday','tuesday')
+# it use for no data change , it is very fast
