5c84f76ff67cd82544ef6a52a39e4e1ea799a875
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2022-12-16 20:49:50 +0530

    Add files via upload

---
+++ class113-more.about.tuple-cipherschool.py
@@ -0,0 +1,43 @@
+mixed=(1,2,3,4,5,6,9)
+
+# loops
+# for i in mixed:
+#     print(i)
+    
+# i=0
+# while i<len(mixed) :
+#     print(mixed[i])
+#     i+=1
+
+# tuple with one element use comma
+# nums=(4,)
+# words=("word1",)
+# print(type(nums))
+# print(type(words))
+
+
+# # tuple also with out bracket
+# x='ram','raj','ravi'
+# print(type(x))
+
+# tuple unpacking
+# alpha=('apple','boy','cat')
+# a,b,c=(alpha)
+# print(b)
+# print(a)
+
+
+# list inside tupple we can add , pop data from that list
+# fav=("apple",'boy',['a','cup',4,5])
+# # fav[2].pop()
+# # fav[2].append('we do it')
+# fav[2].extend('hey')
+# print(fav)
+
+
+# funcn---> min,max,sum,len
+a=(1,2,3,4,5)
+print(min(a))
+print(max(a))
+print(sum(a))
+print(len(a))
