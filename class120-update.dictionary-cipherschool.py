debcb41c7c757d7ec0c14cf8c432978445946609
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2022-12-17 23:47:42 +0530

    Add files via upload

---
+++ class120-update.dictionary-cipherschool.py
@@ -0,0 +1,15 @@
+user_info={
+    'name':'raj',
+    'age':19,
+    'movie':['kgf','rrr','mca'],
+    'song':['humnava','let me']
+}
+
+# more={'state':'odisha','country':'india','pin':761003,'hobbies':['coding','reading','playing']}
+more={'name':'rajesh nahak','state':'odisha','country':'india','pin':761003,'hobbies':['coding','reading','playing']} #it update old 
+
+# user_info.update(more)
+# print(user_info)
+
+user_info.update({})
+print(user_info) #kuchh add nhi hoga old hi return hoga
