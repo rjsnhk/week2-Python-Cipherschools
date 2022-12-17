dbab97f0f7931c7a772846785c5c2f7c194b4a9c
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2022-12-17 18:17:39 +0530

    Add files via upload

---
+++ class117-intro.dictionaries-cipherschool.py
@@ -0,0 +1,57 @@
+# how write dictionary
+# method 1
+# user={"name":'rajesh','age':24}
+# print(user)
+# print(type(user))
+
+# method 2
+# users=dict(name='rajesh',age=19)
+# print(users)
+# print(type(user))
+
+# data access
+# print(users['name'])
+# print(users['age'])
+
+# anything store in dict--num,string,list,dict
+
+# user_info={'name': 'raj', 'age': 19, 'movie': ['kgf', 'rrr', '
+# mca'], 'song': ['humnava', 'let me']}
+
+# user_info={
+#     'name':'raj',
+#     'age':19,
+#     'movie':['kgf','rrr','mca'],
+#     'song':['humnava','let me']
+# }
+# print(user_info)
+# print(user_info['movie'])
+
+
+# dict ke under dict
+# users={
+#     user1:{
+#         'name':'raj',
+#     'age':19,
+#     'movie':['kgf','rrr','mca'],
+#     'song':['humnava','let me']
+#     },
+#     user2:{
+#         'name':'ram',
+#     'age':24,
+#     'movie':['cbi','cid','bahubali'],
+#     'song':['manohari','chill']
+#     }
+# }
+
+
+
+# add data to empty dict
+
+a={}
+a['name']='rajes'
+a['age']=19
+a['place']='berhampur'
+a['vill']='sunathar'
+print(a)
+
