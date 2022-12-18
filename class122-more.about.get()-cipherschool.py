3959601078f2b0eff8bec510042678fba535abc9
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2022-12-18 23:15:44 +0530

    Add files via upload

---
+++ class122-more.about.get()-cipherschool.py
@@ -0,0 +1,5 @@
+# user={'name':'rajesh','age':24}
+# print(user.get('names','not found !')) #none ke jagah not found ayega
+
+user={'name':'rajesh','age':24,'age':14}
+print(user)  #{'name': 'rajesh', 'age': 14} last wala ayega
